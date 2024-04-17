2843cec6a2a2bc217a185a713920974768815eb9
Luna-Porto <145490172+luna-porto@users.noreply.github.com>
2024-04-16 22:12:19 -0300

    Add files via upload

---
+++ aula-16-04/lista-get.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "<br>";
+    echo "o nome é" . $_GET['nome'];
+    echo "<br>";
+    echo "a idade digitada foi". $_GET['idade'];
+    echo "<br>";
+    echo "o endereço é" . $_GET['endereco'];
+    echo "<br>";
+    echo "o cpf é" . $_GET['cpf'];
+
+
+?>
---
+++ aula-16-04/lista-post.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "<br>";
+    echo "o nome é" . $_POST['nome'];
+    echo "<br>";
+    echo "a idade digitada foi". $_POST['idade'];
+    echo "<br>";
+    echo "o endereço é" . $_POST['endereco'];
+    echo "<br>";
+    echo "o cpf é" . $_POST['cpf'];
+
+
+?>
