5abf251db1807420e3cad8cbe54755f3b1169c1f
Luna-Porto <145490172+luna-porto@users.noreply.github.com>
2024-05-07 14:14:02 -0300

    Add files via upload

---
+++ exercicio01/index.php
@@ -0,0 +1,95 @@
+<?php
+
+echo "Aluno(a)";
+echo "<br>";
+
+$aluno1 = array ("Nome"=>"Luna", "Idade"=>17, "CPF"=> "210.378" , "Endereço"=> "Rua Novo Horizonte", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do aluno(a): " .$aluno1['Nome'];
+echo "<br>";
+echo " A idade de Luna é " .$aluno1['Idade'];
+echo "<br>";
+echo " O CPF é:" .$aluno1['CPF'];
+echo "<br>";
+echo "Endereço:" .$aluno1['Endereço'];
+echo "<br>";
+echo "Estado:" .$aluno1['Estado'];
+echo "<br>";
+echo "<br>";
+
+$aluno2 = array ("Nome"=>"Ryan", "Idade"=>17, "CPF"=> "145.345" , "Endereço"=> "Rua dois de julho", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do aluno(a): " .$aluno2['Nome'];
+echo "<br>";
+echo " A idade de Ryan é " .$aluno2['Idade'];
+echo "<br>";
+echo " O CPF é:" .$aluno2['CPF'];
+echo "<br>";
+echo "Endereço:" .$aluno2['Endereço'];
+echo "<br>";
+echo "Estado:" .$aluno2['Estado'];
+echo "<br>";
+echo "<br>";
+
+$aluno3 = array ("Nome"=>"Clara", "Idade"=>18, "CPF"=> "345.190" , "Endereço"=> "Rua Belo Campo", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do aluno(a): " .$aluno3['Nome'];
+echo "<br>";
+echo " A idade de Clara é " .$aluno3['Idade'];
+echo "<br>";
+echo " O CPF é:" .$aluno3['CPF'];
+echo "<br>";
+echo "Endereço:" .$aluno3['Endereço'];
+echo "<br>";
+echo "Estado:" .$aluno3['Estado'];
+echo "<br>";
+echo "<br>";
+
+
+echo "Professor(a)";
+echo "<br>";
+
+$professor1 = array ("Nome"=>"Fabio", "Idade"=>41, "CPF"=> "113.217" , "Endereço"=> "Rua 12 de outubro", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do professor(a): " .$professor1['Nome'];
+echo "<br>";
+echo " A idade de Fabio é " .$professor1['Idade'];
+echo "<br>";
+echo " O CPF é:" .$professor1['CPF'];
+echo "<br>";
+echo "Endereço:" .$professor1['Endereço'];
+echo "<br>";
+echo "Estado:" .$professor1['Estado'];
+echo "<br>";
+echo "<br>";
+
+
+$professor2 = array ("Nome"=>"Queila", "Idade"=>37, "CPF"=> "269.281" , "Endereço"=> "Rua Juliana Castro", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do professor(a): " .$professor2['Nome'];
+echo "<br>";
+echo " A idade de Quila é " .$professor2['Idade'];
+echo "<br>";
+echo " O CPF é:" .$professor2['CPF'];
+echo "<br>";
+echo "Endereço:" .$professor2['Endereço'];
+echo "<br>";
+echo "Estado:" .$professor2['Estado'];
+echo "<br>";
+echo "<br>";
+
+
+$professor3 = array ("Nome"=>"Fernanda", "Idade"=>43, "CPF"=> "678.217" , "Endereço"=> "Rua Kelvin Cotrim", "Estado"=> "Bahia");
+echo "<br>";
+echo " Nome do professor(a): " .$professor3['Nome'];
+echo "<br>";
+echo " A idade de Fernanda é " .$professor3['Idade'];
+echo "<br>";
+echo " O CPF é:" .$professor3['CPF'];
+echo "<br>";
+echo "Endereço:" .$professor3['Endereço'];
+echo "<br>";
+echo "Estado:" .$professor3['Estado'];
+echo "<br>";
+echo "<br>";
+?>
